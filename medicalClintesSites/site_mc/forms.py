f52b874496aec06370adff0f1a33344235aba0d7
Crhisllane <crhisllane@gmail.com>
2019-09-29 01:41:16 -0300

    Melhorias visuais

--- medicalClintesSites/site_mc/forms.py
+++ medicalClintesSites/site_mc/forms.py
@@ -2,10 +2,10 @@ from django import forms
 from datetime import datetime
 
 class ClienteForm(forms.Form):
-    nome = forms.CharField()
-    dataNascimento = forms.DateField(input_formats=['%d-%m-%Y'])
-    dataColeta = forms.DateField(input_formats=['%d-%m-%Y'])
-    dataEntrega = forms.DateField(input_formats=['%d-%m-%Y'])
-    CRM = forms.CharField()
+    nome = forms.CharField(widget=forms.TextInput(attrs={'class' : 'uk-input'}))
+    dataNascimento = forms.DateField(label="Data Nascimento",input_formats=['%d-%m-%Y'], widget=forms.DateInput(attrs={'class' : 'uk-input'}))
+    dataColeta = forms.DateField(label="Data Coleta",input_formats=['%d-%m-%Y'], widget=forms.DateInput(attrs={'class' : 'uk-input'}))
+    dataEntrega = forms.DateField(label="Data Entrega",input_formats=['%d-%m-%Y'], widget=forms.DateInput(attrs={'class' : 'uk-input'}))
+    CRM = forms.CharField(label="CRM do médico solicitante",widget=forms.TextInput(attrs={'class' : 'uk-input'}))
 
 
